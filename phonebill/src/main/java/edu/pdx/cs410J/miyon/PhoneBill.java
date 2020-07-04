40548f38b9ca42d77f46566095d31ac2e266c727
Mi Yon Kim <miyon@pdx.edu>
2020-07-04 00:21:04 -0700

    Added the PhoneBill class

---
+++ phonebill/src/main/java/edu/pdx/cs410J/miyon/PhoneBill.java
@@ -0,0 +1,22 @@
+package edu.pdx.cs410J.miyon;
+
+import edu.pdx.cs410J.AbstractPhoneBill;
+
+import java.util.Collection;
+
+public class PhoneBill extends AbstractPhoneBill<PhoneCall> {
+    @Override
+    public String getCustomer() {
+        throw new UnsupportedOperationException("This method is not implemented yet");
+    }
+
+    @Override
+    public void addPhoneCall(PhoneCall phoneCall) {
+        throw new UnsupportedOperationException("This method is not implemented yet");
+    }
+
+    @Override
+    public Collection<PhoneCall> getPhoneCalls() {
+        throw new UnsupportedOperationException("This method is not implemented yet");
+    }
+}

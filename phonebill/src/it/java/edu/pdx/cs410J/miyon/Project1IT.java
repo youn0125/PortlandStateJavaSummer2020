9e34ba906f10f3975b192366567ce988b4623b9e
Mi Yon Kim <miyon@pdx.edu>
2020-07-05 19:16:15 -0700

    Check phone Number and date format with regex. Add itTest with valid five arguments.

--- phonebill/src/it/java/edu/pdx/cs410J/miyon/Project1IT.java
+++ phonebill/src/it/java/edu/pdx/cs410J/miyon/Project1IT.java
@@ -19,14 +19,74 @@ public class Project1IT extends InvokeMainTestCase {
         return invokeMain( Project1.class, args );
     }
 
-  /**
-   * Tests that invoking the main method with no arguments issues an error
-   */
-  @Test
-  public void testNoCommandLineArguments() {
-    MainMethodResult result = invokeMain();
-    assertThat(result.getExitCode(), equalTo(1));
-    assertThat(result.getTextWrittenToStandardError(), containsString("Missing command line arguments"));
-  }
+   /**
+    * Tests that invoking the main method with no arguments issues an error
+    */
+   @Test
+   public void testNoCommandLineArguments() {
+     MainMethodResult result = invokeMain();
+     assertThat(result.getExitCode(), equalTo(1));
+     assertThat(result.getTextWrittenToStandardError(), containsString("Missing command line arguments"));
+   }
+    /**
+     * Tests that invoking the main method with no arguments issues an error
+     */
+    @Test
+    public void testOneCommandLineArguments() {
+        MainMethodResult result = invokeMain("Brian Griffin");
+        assertThat(result.getExitCode(), equalTo(1));
+        assertThat(result.getTextWrittenToStandardError(), containsString("Missing command line arguments"));
+    }
+    private PhoneCall createPhoneCall() {
+        String callee = "123-456-7890";
+        String caller = "234-567-8901";
+        String start = "01/23/2020 09:12";
+        String end = "01/23/2020 10:12";
+        return new PhoneCall(caller, callee, start, end);
+    }
 
+    /**
+     * Tests that invoking the main method with no arguments issues an error
+     */
+    @Test
+    public void testTwoCommandLineArguments() {
+        MainMethodResult result = invokeMain("Brian Griffin", "123-456-7890");
+        assertThat(result.getExitCode(), equalTo(1));
+        assertThat(result.getTextWrittenToStandardError(), containsString("Missing command line arguments"));
+    }
+    /**
+     * Tests that invoking the main method with no arguments issues an error
+     */
+    @Test
+    public void testThreeCommandLineArguments() {
+        MainMethodResult result = invokeMain("Brian Griffin", "123-456-7890", "234-567-8901");
+        assertThat(result.getExitCode(), equalTo(1));
+        assertThat(result.getTextWrittenToStandardError(), containsString("Missing command line arguments"));
+    }
+    /**
+     * Tests that invoking the main method with no arguments issues an error
+     */
+    @Test
+    public void testFourCommandLineArguments() {
+        MainMethodResult result = invokeMain("Brian Griffin", "123-456-7890", "234-567-8901", "01/23/2020 09:12");
+        assertThat(result.getExitCode(), equalTo(1));
+        assertThat(result.getTextWrittenToStandardError(), containsString("Missing command line arguments"));
+    }
+
+    /**
+     * Tests that invoking the main method with no arguments issues an error
+     */
+    @Test
+    public void testFiveCommandLineArgumentsWithValidArgs() {
+        String customer = "Brian Griffin";
+        String callee = "123-456-7890";
+        String caller = "234-567-8901";
+        String start = "01/23/2020 09:12";
+        String end = "01/23/2020 10:12";
+        MainMethodResult result = invokeMain(customer, callee, caller, start, end);
+        PhoneCall call = new PhoneCall(callee, caller, start, end);
+        PhoneBill bill = new PhoneBill(customer);
+        bill.addPhoneCall(call);
+        assertThat (result.getTextWrittenToStandardOut(), containsString("Phone call is added to Phone bill"));
+    }
 }
--- phonebill/src/main/java/edu/pdx/cs410J/miyon/Project1.java
+++ phonebill/src/main/java/edu/pdx/cs410J/miyon/Project1.java
@@ -1,17 +1,57 @@
 package edu.pdx.cs410J.miyon;
 
+import java.util.regex.*;
+
 /**
  * The main class for the CS410J Phone Bill Project
  */
 public class Project1 {
 
   public static void main(String[] args) {
-    PhoneCall call = new PhoneCall(null,null, null, null);  // Refer to one of Dave's classes so that we can be sure it is on the classpath
-    System.err.println("Missing command line arguments");
-    for (String arg : args) {
-      System.out.println(arg);
+    if ( args.length > 4) {
+      boolean chkPNumber = checkPNumberPatten(args[1]);
+      chkPNumber = checkPNumberPatten(args[2]);
+      boolean chkDate = checkDatePattern(args[3]);
+      chkDate = checkDatePattern(args[4]);
+
+      if (chkPNumber && chkDate) {
+        PhoneBill bill = new PhoneBill(args[0]);
+        PhoneCall call = new PhoneCall(args[1], args[2], args[3], args[4]);  // Refer to one of Dave's classes so that we can be sure it is on the classpath
+        bill.addPhoneCall(call);
+        System.out.println("Phone call is added to Phone bill");
+      } else {
+        System.err.println("Check arguments' pattern");
+        for (String arg : args) {
+          System.out.println(arg);
+        }
+        System.exit(1);
+      }
+
     }
-    System.exit(1);
+    else {
+      System.err.println("Missing command line arguments");
+      for (String arg : args) {
+        System.out.println(arg);
+      }
+      System.exit(1);
+    }
+
   }
 
+  public static boolean checkPNumberPatten(String pNumber) {
+    String pattern = "(?:\\d{3}-){2}\\d{4}";
+    if (pNumber.matches(pattern)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  public static boolean checkDatePattern(String date) {
+    String pattern = "^\\d{1,2}\\/\\d{1,2}\\/\\d{4} \\d{1,2}:\\d{1,2}";
+    if (date.matches(pattern)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }

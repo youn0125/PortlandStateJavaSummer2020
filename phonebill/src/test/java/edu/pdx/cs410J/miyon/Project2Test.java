d72989f62cb1e2bd48be24b11b8ba211cbafa371
Mi Yon Kim <miyon@pdx.edu>
2020-07-15 14:25:57 -0700

    Add Project2Test class

---
+++ phonebill/src/test/java/edu/pdx/cs410J/miyon/Project2Test.java
@@ -0,0 +1,31 @@
+package edu.pdx.cs410J.miyon;
+
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * A unit test for code in the <code>Project2</code> class.  This is different
+ * from {@link Project2IT} which is an integration test (and can handle the calls
+ * to {@link System#exit(int)} and the like.
+ */
+public class Project2Test {
+
+    @Test
+    public void readmeCanBeReadAsResource() throws IOException {
+        try (
+                InputStream readme = Project2.class.getResourceAsStream("README.txt");
+        ) {
+            assertThat(readme, not(nullValue()));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(readme));
+            String line = reader.readLine();
+            assertThat(line, containsString("This README is written by Mi Yon Kim and referred from Project2 AppClasses document"));
+        }
+    }
+}
